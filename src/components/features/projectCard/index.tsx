f72b5e3891c5591e9d621711b15c82f977f84597
Misato Tanno <breitmeer.0804@gmail.com>
2024-06-10 21:20:51 -0600

    Modefied entities

--- src/components/features/projectCard/index.tsx
+++ src/components/features/projectCard/index.tsx
@@ -22,7 +22,7 @@ export const ProjectCard = () => {
           <div className='py-12'>
             <h6>PROJECT</h6>
             <h1 className='font-display font-bold my-2'>This is my Project</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             </p>
           </div>
           <div className='grid w-full grid-cols-3 gap-y-1.5'>
