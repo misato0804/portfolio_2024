60a127b198f0353e0afd8246ad2e813c86ed26cb
Misato Tanno <breitmeer.0804@gmail.com>
2024-06-11 23:13:19 -0600

    Re pushed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
       <main className="">
         <MainTop/>
         <MainProjects/>
+
         {/*  About me  */}
         {/*  Works  */}
         {/*  Contact  */}
---
+++ src/components/features/3D/Particles/index.tsx
@@ -0,0 +1,43 @@
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Points, PointMaterial } from '@react-three/drei';
+import * as THREE from 'three';
+
+export const Particles = () => {
+  const ref = useRef<THREE.Points>(null!);
+
+  //
+  const particles = new THREE.BufferGeometry();
+  const count = 1000
+  const positions = new Float32Array(count * 3);
+
+  const colors = new Float32Array(count * 3);
+
+  for (let i = 0; i < count * 3; i++) {
+    positions[i] = Math.random()
+
+    colors[i * 3] = 1.0;
+    colors[i * 3 + 1] = 0.4;
+    colors[i * 3 + 2] = 0.6;
+  }
+
+  particles.setAttribute('position', new THREE.Int8BufferAttribute(positions, 3))
+  particles.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+  useFrame(() => {
+    if (ref.current) {
+      ref.current.rotation.y += 0.002;
+    }
+  });
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({ color: 'red' })
+
+  const mesh = new THREE.Mesh()
+
+  return (
+      <Points ref={ref} geometry={particles}>
+        <PointMaterial size={0.1} vertexColors={true}/>
+      </Points>
+  );
+}
