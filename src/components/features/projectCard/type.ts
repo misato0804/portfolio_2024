3d062ad4c00caee7693a244c10836924496f9292
Misato Tanno <breitmeer.0804@gmail.com>
2024-07-12 13:11:42 -0600

    Added animation to horizontal scroll

--- src/components/features/projectCard/type.ts
+++ src/components/features/projectCard/type.ts
@@ -1,7 +1,19 @@
 export type ProjectCardProps = {
   title: string,
   image: string,
+  alt: string,
   demoLink: string,
+  githubLink: string,
+  description: string,
+  stacks: string[]
+}
+
+export type Project = {
+  title: string,
+  image: string,
+  alt: string,
+  demoLink: string,
+  githubLink: string,
   description: string,
   stacks: string[]
 }
