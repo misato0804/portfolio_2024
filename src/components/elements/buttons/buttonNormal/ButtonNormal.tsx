b5affd90027718066fd6aa933227743a0177004f
Misato Tanno <breitmeer.0804@gmail.com>
2024-08-15 21:52:52 +0900

    modified links

--- src/components/elements/buttons/buttonNormal/ButtonNormal.tsx
+++ src/components/elements/buttons/buttonNormal/ButtonNormal.tsx
@@ -1,11 +1,24 @@
 import { Button } from "@/components/ui/button"
 import { ButtonProps } from "./type"
+import Link from "next/link"
 
-const ButtonNormal = ({title, icon: IconComponent, onClick, bgColor} : ButtonProps) => {
+const ButtonNormal = ({ title, icon: IconComponent, onClick, bgColor, link }: ButtonProps) => {
+
+  if (link) {
+    return (
+      <Link href={link} target="_blank">
+        <Button onClick={onClick} className={`${bgColor} py-6 px-6 text-[1.4rem]`}>
+          {IconComponent ? <IconComponent size={18} className='mr-2' /> : null}
+          {title}
+        </Button>
+      </Link>
+    )
+  }
+
   return (
     <Button onClick={onClick} className={`${bgColor} py-6 px-6 text-[1.4rem]`}>
-      { IconComponent ? <IconComponent size={18} className='mr-2'/> : null }
-        { title }
+      {IconComponent ? <IconComponent size={18} className='mr-2' /> : null}
+      {title}
     </Button>
   )
 }
